a547f616af4b766ae71dcfdda3717a67f7c9e5de
Josh LaBounty <jjlab@uw.edu>
2020-07-18 05:45:35 -0700

    Adds visualization functions reimplemented from python_fit -> fitVector function

--- g2luigi/g2Fitter.py
+++ g2luigi/g2Fitter.py
@@ -98,6 +98,7 @@ class g2Fitter():
     def cost_functons(self,name):
         dict_cost_functons = {
             "LeastSquares":LeastSquares
+            #TODO: #2 Add custom minimizer which takes advantage of the kernel method
         }
         return dict_cost_functons[name]
 
@@ -149,7 +150,7 @@ class g2Fitter():
             self.yerr = self.h.view().variance[self.binlims[0]:self.binlims[1]]
         else:
             self.yerr = None 
-            self.xerr = None
+        self.xerr = None
 
         # self.fit_function = self.fit_functions[self.fit_name]
         if(self.fit_name is not "custom"):
@@ -184,6 +185,20 @@ class g2Fitter():
         else:
             #for the iterative fit, loop over the parameters and unmask them iteratively using the same minuit object
             self.iterative_fit(nFit)
+
+        self.compute_residuals()
+
+    def compute_residuals(self):
+
+        #compute all residuals and pulls
+        self.residuals = self.y - self.fity
+        self.pulls = np.divide((self.y - self.fity), (self.yerr), out=np.zeros_like(self.y), where=self.yerr!=0)
+
+        #compute the ones which lie in the fit range
+        self.residualsInRange = self.residuals[ self.h.axes[0].index(self.xlims[0]):self.h.axes[0].index(self.xlims[1]) ]
+        self.pullsInRange = self.pulls[ self.h.axes[0].index(self.xlims[0]):self.h.axes[0].index(self.xlims[1])  ]
+
+        print("Residuals computed!")
 
     def iterative_fit(self, nFit=2):
         print("Starting iterative fit")
@@ -233,6 +248,7 @@ class g2Fitter():
 
             print(self.m.params)
 
+
         print("All done!")
 
     def do_single_fit(self, nFit=2):
@@ -281,6 +297,9 @@ class g2Fitter():
 
         print(self,"can now be pickled")
 
+    # --------------------------------------------------------
+    # Plotting utility functions
+    # 
 
     def plot_result(self):
         fig,ax = plt.subplots(figsize=(15,5))
@@ -295,26 +314,265 @@ class g2Fitter():
         plt.show()
         return(fig,ax)
 
+    def labelFit(self, parNames=None, functionString=None, formatStr="7.3E", func=None,fitname=None):
+        # import ROOT as r
+        import textwrap
+        import ctypes 
 
-# test output
-# infile = "../root_import/data/test2_clustersAndCoincidences_corrected_[['y', 2100, 2200]].pickle"
-# input_hist = pickle.load(open(infile,"rb"))['clustersAndCoincidences/corrected']
+        pars = self.values
+        parErrs = self.errors
+        chiSquare = self.chiSquare
 
-# print(type(input_hist))
+        if(self.parNames is None):
+            parNames = ["p"+str(i) for i in range(len(pars))]
+            #parNames = [self.convertRootLabelsToPython(str(self.f.GetParName(i))) for i in range(len(pars))]
+        else:
+            parNames = self.parNames
 
-# ding = g2Fitter("5par","LeastSquares","test", input_hist, [1090000,64.4,0.33,0,-1.2], [30,300])
-# print(ding, type(ding))
-# print(ding.fit_function)
-# print(ding.fit_function(np.array([10,11,12]),[10,1,0.33,0,0]))
+        if(self.xlims is not None):
+            parstring = "Fit Result ["+str(round(self.xlims[0],2))+" - "+str(round(self.xlims[1],2))+"]"+"\n"
+        else:
+            parstring = "Fit Result"+"\n"
 
-# ding.do_fit()
-# # ding.plot_result()
+        if(fitname is not None):
+            parstring += str(fitname)+"\n"
 
-# pickle.dump(ding,open("./test_fit_output.pickle","wb"), protocol=2)
-# ding2 = pickle.load(open("./test_fit_output.pickle","rb"))
-# print(ding2)
+        if(functionString is not None):
+            wrapped = textwrap.wrap(functionString, 25)
+            if(len(wrapped) > 0):
+                parstring+="Function: "+wrapped[0]+"\n"
+            if(len(wrapped)>1):
+                for fi in wrapped[1:]:
+                    parstring += "                "+fi+"\n"
+        for i in range(len(pars)):
+            #parstring += str(parNames[i])+"\t= "+str(format(pars[i], formatStr))
+            if(parErrs is not None):
+                parstring += ("{0}".ljust(10)+" = {1:.3e} $\\pm$ {2:.3e}").format(parNames[i], pars[i], parErrs[i])+"\n"
+                #parstring += "\t"+r"$\pm$ "+str(format(parErrs[i], formatStr))+"\n"
+            else:
+                parstring += ("{0}".ljust(10)+" = {1:.3e}").format(parNames[i], pars[i])+"\n"
+                #parstring += "\n"
+        if(chiSquare is not None):
+            parstring += r"$\chi^{2}/NDF$ = "+str(chiSquare)
+            
+        # parstring="Placeholder"
 
-# ding2.load_fit()
-# print(ding2.m.values)
+        return parstring
 
-# print("All done!")
+
+    def draw(self,  title = "Fit Result", yrange=[None, None], xrange=None, data_title = "Data", 
+                    resid_title = "Fit Pulls", do_pulls=True, fit_hist=True, fmti=".-", 
+                    draw_confidence_intervals=False):
+        '''
+            Creates a figure in matplotlib and draws the fitresult / residuals on it
+        '''
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        #fig, axs = plt.subplots(2,1,figsize=(15,8), sharex=True)
+        fig = plt.figure(figsize=(15,8))
+        gs = fig.add_gridspec(2,3)
+        ax1 = fig.add_subplot(gs[0, :])
+        ax2 = fig.add_subplot(gs[1, :-1])
+        ax3 = fig.add_subplot(gs[1, -1:])
+
+        axs = [ax1, ax2, ax3]
+        ax = axs[0]
+        ax.errorbar(self.x, self.y, xerr=self.xerr, yerr=self.yerr, fmt=fmti, 
+                    label="Data", ecolor='xkcd:grey', zorder=35)
+        
+        self.drawFitResult(ax, scaleFactor=int(len(self.x)*10))
+        #TODO: #3 Re-implement confidence intervals
+        # if(draw_confidence_intervals):
+        #     self.drawConfidenceIntervals(ax,"blue", "95% Confidence Level")
+        ax.grid()
+        if(yrange[0] is None):
+            ymean = np.mean(self.y)
+            ystd = np.std(self.y)
+            ax.set_ylim(ymean - ystd*2, ymean + ystd*2)
+        else:
+            ax.set_ylim(yrange[0][0],yrange[0][1])
+        if(xrange is not None):
+            ax.set_xlim(xrange[0], xrange[1])
+        leg = ax.legend(ncol=1, loc=4)
+        leg.set_zorder(37)
+        ax.set_ylabel(data_title)
+
+        ax = axs[1]
+        if(do_pulls and (self.yerr is not None)):
+            ax.set_title("Fit Pulls")
+            self.plotResiduals(ax,1,fmti,"All Residuals",1)
+        else:
+            ax.set_title("Fit Residuals")
+            self.plotResiduals(ax,0,fmti,"All Residuals",1)
+        ax.set_ylabel(resid_title)
+        ax.grid()
+        if(yrange[1] is None):
+            ymean = np.mean(self.residuals)
+            ystd = np.std(self.residuals)
+            ax.set_ylim(ymean - ystd*2, ymean + ystd*2)
+        else:
+            ax.set_ylim(yrange[1][0],yrange[1][1])
+        #ax.set_ylim(-10,10)
+        if(xrange is not None):
+            ax.set_xlim(xrange[0], xrange[1])
+        
+        ax = axs[2]
+        ax.grid()
+            
+        if(do_pulls and (self.yerr is not None)):
+            ax.set_title("Histogram of Fit Pulls")
+            hist1 = ax.hist(self.pullsInRange, bins=41)
+        else:
+            ax.set_title("Histogram of Fit Residuals")
+            hist1 = ax.hist(self.residualsInRange, bins=41)
+            
+        # if(fit_hist):
+        #     fitHistGaussian(hist1, ax, True, True)
+        #     ax.legend()
+
+        plt.suptitle(title, y=1.02, fontsize=18)
+        plt.tight_layout()
+        
+        return (fig, axs)
+
+    def drawFitResult(self, ax, scaleFactor=100, drawFormat="-", label="Default", 
+                      color="xkcd:orange", fitname=None, xrange=None):
+        #print(fitresult)
+        '''
+            Draws a fit result object returned by fitVector function on 'ax' (axes or plot from matplotlib)
+        '''
+        import numpy as np
+        #from standardInclude import labelFit
+
+        if(xrange is not None):
+            x0 = xrange[0]
+            xn = xrange[1]
+            xs = np.linspace(x0,xn,scaleFactor)
+            ys = self.fit_function(xs, self.values)
+        elif(scaleFactor > 1):
+            x0 = self.x[0]
+            xn = self.x[len(self.x)-1]
+            xs = np.linspace(x0,xn,scaleFactor)
+            ys = self.fit_function(xs, self.values)
+
+        else:
+            xs = self.x
+            ys = self.fity
+        
+        if(label == "Default"):
+            labelString = self.labelFit( None, "Test", "7.3E", None, fitname=None) 
+        else:
+            labelString = label
+        
+        ax.plot(xs,ys,drawFormat,label=labelString, zorder=36, color=color)
+        
+
+    def plotResiduals(self, ax, to_plot = 0, fmt=".",labeli="Fit Residuals", runningAverage = 1):
+        '''
+            Inputs:
+                ax             - axis on which to plot
+                to_plot        - 0 = residuals; 1 = pulls
+                format         - string of formats
+                labeli         - what to put in the legend
+                runningAverage - Number of points around the residual to compute the running average for
+            Outputs:
+                matplotlib object containing residuals            
+        '''
+        import numpy as np
+
+        x = self.x
+        if(to_plot == 1):
+            resid = self.pulls
+        else:
+            resid = self.residuals
+        
+        resid_to_plot = []
+        if(runningAverage > 1):
+            #resid = [np.nan for i in range(runningAverage)]+resid+[np.nan for i in range(runningAverage)]
+            for i in range(len(x)):
+                #ri = resid[i]
+                if((i >= runningAverage) and ((len(x) - i) >= runningAverage)):
+                    mean_ri = np.nanmean(resid[i-runningAverage:i+runningAverage])
+                else:
+                    mean_ri = np.nan
+                resid_to_plot.append(mean_ri)
+        else:
+            resid_to_plot = resid
+        
+        #print(resid_to_plot)
+        ding = ax.plot(x,resid_to_plot,fmt,label=labeli)
+        return ding
+
+    def plotRunningAverage(self, averages=[1,10,100], showplot=True, fmti="."):
+        import matplotlib.pyplot as plt  
+        fig,ax = plt.subplots(figsize=(15,5))
+        for average in averages:
+            self.plotResiduals(ax, 1, fmti, str(average), average)
+        plt.grid()
+        plt.title("Running Average of Residuals")
+        plt.legend()
+        return (fig,ax)
+
+    def fft(self, xrange=None, option=0, time_conversion_factor = 10**(-6), drawHist = True, logy=True):
+        '''
+            Performs an FFT using python on the functions stored in this class. 
+            Options:
+                0 = Input function
+                1 = Residuals
+                2 = Pulls
+            xrange: Range over which to perform the FFT. Can be used to exclude data points at beginning/end.
+            time_conversion_factor: conversion factor to get the x axis time units to seconds
+            drawHist: whether or not to draw the histogram
+            logy : Should the fft axis be a log scale
+        '''
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        if(option == 0):
+            points_raw = self.y
+        elif(option == 1):
+            points_raw = self.residuals
+        elif(option == 2):
+            points_raw = self.pulls
+        else:
+            raise ValueError("Invalid option in fft: "+str(option))
+
+        if(xrange is not None):
+            points = []
+            xs = []
+            print("Restricting range of FFT to:", xrange)
+            if(len(xrange) is not 2):
+                raise ValueError("xrange is not well defined")
+            for i, xi in enumerate(self.x):
+                if(xi >= xrange[0] and xi <= xrange[1]):
+                    points.append(points_raw[i])
+                    xs.append(xi)
+        else:
+            xs = self.x
+            points = points_raw
+
+        ding = np.fft.fft(np.array(points))
+        n = len(points)
+        d = xs[1] - xs[0]
+        freq = np.fft.fftfreq(n, d)
+        if(drawHist):
+            fig, ax = plt.subplots(1,2,figsize=(15, 5))
+            for axi in ax:
+                axi.grid()
+            ax[0].plot(xs,points,"-")
+            ax[0].set_title("Input function")
+            ax[1].set_title("FFT result")
+            ax[1].plot([np.abs(x) for x in freq],np.abs(ding),'.:',label='FFT Result')
+            #ax[1].set_xlim(0,50)
+            if(logy):
+                ax[1].set_yscale("log")
+            plt.xlabel("Frequency [MHz]")
+            #plt.xlim(0,6.7157787731503555 / 2)# *10.**6)
+            plt.legend()
+            plt.suptitle("FFT Of Fit Result", y=1.02, fontsize=18)
+            plt.tight_layout()
+            plt.show()
+
+        return (freq, ding)
